9a3c860fbe8b9b0cec402d5aab7981811da8b298
xianyi <2870005947@qq.com>
2025-06-07 11:15:13 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <script setup>
+import { nextTick } from "vue";
 import Top from "./views/Top.vue";
+const a = ref("123");
+const hhh = () => {
+  console.log("同步代码");
+  setTimeout(() => {
+    console.log("宏任务");
+  });
+
+  a.value += a.value;
+
+  nextTick(() => {
+    console.log("nextTick");
+  });
+    Promise.resolve(1).then(() => {
+    console.log("Promise");
+  });
+};
 </script>
 
 <template>
   <div class="container">
+    <button @click="hhh">测试{{ a }}</button>
     <n-message-provider>
       <n-dialog-provider>
         <Top />
       </n-dialog-provider>
 
-     
-        <router-view> </router-view>
-  
+      <router-view> </router-view>
     </n-message-provider>
   </div>
 </template>
